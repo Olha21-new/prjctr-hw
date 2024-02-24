c0e02a45846786b39c0ec322ed51014ebc6d28b1
Olya308 <121465155+Olha21-new@users.noreply.github.com>
2024-02-24 21:03:47 +0200

    Update homework_4.js
    Updated task 1 to have one line of code

--- homework_4.js
+++ homework_4.js
@@ -4,11 +4,7 @@
 const currentMaxValue = 4589;
 let reverseMaxValue;
 
-let stringValue = currentMaxValue.toString();
-let charArray = stringValue.split("");
-let reversedArray = charArray.reverse();
-let reversedString = reversedArray.join("");
-reverseMaxValue = parseInt(reversedString);
+let reverseMaxValue = currentMaxValue.toString().split("").reverse().join("");
 
 console.log(reverseMaxValue); // 9854
 console.log(typeof reverseMaxValue); // 'number'
