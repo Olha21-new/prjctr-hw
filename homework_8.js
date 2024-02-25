7000f941856879311c430649753202a33af02e60
Olya308 <121465155+Olha21-new@users.noreply.github.com>
2024-02-25 11:47:42 +0200

    Create homework_8.js

---
+++ homework_8.js
@@ -0,0 +1,22 @@
+// Task 1
+function addThemAll(...args) {
+  let sum = [...args].reduce(function (a, b) {
+    return a + b;
+  });
+  console.log(sum);
+}
+
+console.log(addThemAll(2, 4)); // 6
+console.log(addThemAll(1, 2, 3, 4)); // 10
+console.log(addThemAll(5, 5, 10)); // 20
+
+// Task 2
+console.log(multiply(5)(5)); // 25
+console.log(multiply(2)(-2)); // -4
+console.log(multiply(4)(3)); // 12
+
+function multiply(a) {
+  return function (b) {
+    return a * b;
+  };
+}
