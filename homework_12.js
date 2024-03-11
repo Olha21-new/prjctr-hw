7c9aef9d43134806cf48914c1e8a4000fc7d30f7
Olya308 <121465155+Olha21-new@users.noreply.github.com>
2024-03-11 22:45:24 +0200

    Create homework_12.js

---
+++ homework_12.js
@@ -0,0 +1,10 @@
+let headerTwo = document.getElementById("headerTwo");
+let section = document.querySelector(".firstSection");
+let listItem = document.querySelector("ul li:nth-child(5)");
+let hatredLevelBlock = document.querySelector(".hatredLevelBlock");
+
+console.log(headerTwo);
+console.log(section);
+console.log(listItem);
+console.log(hatredLevelBlock);
+
